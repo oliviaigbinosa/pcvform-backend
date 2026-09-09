bae24470f5d484fe3d94d78628fd3ab88ac26ed8
Olivia Igbinosa <oi506332@gmail.com>
2026-09-09 12:36:46 +0100

    fix cors

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(helmet())
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://pettycashvoucher.netlify.app',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000/',
   credentials: true
 }))
 
